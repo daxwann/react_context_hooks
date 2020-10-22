9e572c4d370705a50d019129bd14ffb8ec8d7f83
Dax Wann <daxwann@gmail.com>
2020-10-21 22:45:49 -0700

    use static contextType to consume context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,29 +1,29 @@
-import React, { createContext, Component } from 'react';
+import React, { createContext, Component } from "react";
 
 export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
-  state = { 
+  state = {
     isLightTheme: true,
     light: {
       textColor: "#555",
       uiColor: "#ddd",
-      bgColor: "#eee"
+      bgColor: "#eee",
     },
     dark: {
       textColor: "#ddd",
       uiColor: "#333",
-      bgColor: "#555"
-    }
-   }
+      bgColor: "#555",
+    },
+  };
 
-  render() { 
-    return ( 
-      <ThemeContext.Provider value={{...this.state}}>
+  render() {
+    return (
+      <ThemeContext.Provider value={{ ...this.state }}>
         {this.props.children}
       </ThemeContext.Provider>
     );
   }
 }
- 
+
 export default ThemeContextProvider;
