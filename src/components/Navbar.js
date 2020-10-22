c6c966e07a0f6162cf13e57d57e02cc60ef499b8
Dax Wann <daxwann@gmail.com>
2020-10-21 22:54:51 -0700

    use context.consumer to access theme state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{background: theme.uiColor, color: theme.textColor}}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+
+          return (
+            <nav style={{ background: theme.uiColor, color: theme.textColor }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
