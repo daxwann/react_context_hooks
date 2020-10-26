4ff232af845d806a1764a9e6d067cf0051c5581a
Dax Wann <daxwann@gmail.com>
2020-10-26 00:31:07 -0700

    add ThemeToggle and change class components to functional components with hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import AuthContextProvider from './contexts/AuthContext';
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <AuthContextProvider>
           <Navbar />
           <BookList />
+          <ThemeToggle />
         </AuthContextProvider>
       </ThemeContextProvider>
     </div>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
 
-  render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-
-    return (
-      <div className='book-list' style={{background: theme.bgColor, color: theme.textColor}}>
-        <ul>
-          <li style={{background: theme.uiColor}}>the way of kings</li>
-          <li style={{background: theme.uiColor}}>the name of the wind</li>
-          <li style={{background: theme.uiColor}}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bgColor, color: theme.textColor }}
+    >
+      <ul>
+        <li style={{ background: theme.uiColor }}>the way of kings</li>
+        <li style={{ background: theme.uiColor }}>the name of the wind</li>
+        <li style={{ background: theme.uiColor }}>the final empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
